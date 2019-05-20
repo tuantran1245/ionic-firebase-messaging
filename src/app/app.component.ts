5b6a4b16490608100e6d0165576792e24805f8dd
Tuantq <tuantran1245@gmail.com>
2019-05-20 23:38:51 +0700

    remove api key from typescript file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,6 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import * as firebase from 'firebase';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyD3kYmK8SmJjY-rxcTTy3Qvt6CEj1KclxU',
-  authDomain: 'tuantq-messaging-app.firebaseapp.com',
-  databaseURL: 'https://tuantq-messaging-app.firebaseio.com',
-  projectId: 'tuantq-messaging-app',
-  storageBucket: 'tuantq-messaging-app.appspot.com',
-  messagingSenderId: '555358170510',
-  appId: '1:555358170510:web:98556684e44f1288'
-};
-
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -33,6 +23,5 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
-    //firebase.initializeApp(firebaseConfig);
   }
 }
